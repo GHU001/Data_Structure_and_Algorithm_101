6df9583a573f055b4b8682a38002bab713328d5e
GUANYANG <ghuu001@gmail.com>
2020-05-07 11:27:52 +0800

    Updated nodelink

--- Notelink.py
+++ Notelink.py
@@ -6,10 +6,16 @@ class Node(object):
 # 带空头节点的链表
 
 class LinkList:
-    def __init__(self, li):
-        self.create_linklist_head(li)
-
-    def create_linklist_head(self, li):
+    def __init__(self, li, method='tail'):
+        self.head = None
+        self. tail = None
+        if method == 'tail':
+            self.create_linklist_tail(li)
+        elif method =='head':
+            self.create_linklist_head(li)
+        else:
+            raise TypeError('Unsupported method')
+    def create_linklist_head(self, li):#头插法，倒叙
         self.head = Node(0)
         for v in li:
             n = Node(v)
@@ -23,10 +29,28 @@ class LinkList:
             print(p.data)
             p = p.next
 
+    def create_linklist_tail(self, li):
+        self.head = Node(0)
+        self.tail = self.head
+        for v in li:
+            p = Node(v)
+            self.tail.next = p
+            self.tail = p
+            self.head.data += 1
+
+    def __len__(self):
+        return self.head.data
+
+
+
+
+
+
 
 if __name__ =="__main__":
-    linknote = LinkList([1,2,3,4,5,6])
+    linknote = LinkList([1,2,3,4,5,6],'tail')
     linknote.travser_linklist()
+    print(len(linknote))
 
 
 
