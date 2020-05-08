fae3effa68702951505cbea5a1e0e7f8a085fb34
GUANYANG <ghuu001@gmail.com>
2020-05-08 23:54:04 +0800

    Updated BitreeNode

--- BiTreeNode.py
+++ BiTreeNode.py
@@ -1,9 +1,21 @@
+from _collections import deque
+
 class BiTreeNode(object):
 
     def __init__(self, data= None):
         self. data = data
         self.lchild = None
         self.rchild = None
+
+A = BiTreeNode('A')
+B = BiTreeNode('B')
+C = BiTreeNode('C')
+D = BiTreeNode('D')
+E = BiTreeNode('E')
+F = BiTreeNode('F')
+G = BiTreeNode('G')
+
+
 
 #前序遍历、中序遍历、后序遍历、层次遍历
 def pre_order(root):
@@ -24,3 +36,14 @@ def in_order(root):
         in_order(root.lchild)
         print(root.data, end='')
         in_order(root.rchild)
+
+def level_order(root):
+    q = deque()
+    q.append(root)
+    while(len(q)):
+        x = q.popleft()
+        print(x.data, end='')
+        if x.lchild:
+            q.append(x.lchild)
+        if x.rchild:
+            q.append(x.rchild)
