6b8386a1eff3df997f2af0e2070823751a486ef0
GHU001 <ghuu001@gmail.com>
2020-05-14 23:49:10 +0800

    Default Changelist

---
+++ quick_sort.py
@@ -0,0 +1,26 @@
+li = [1,3,2,4,1,5,5,4,3]
+
+a = 0
+for i in li:
+    a = a ^ i
+print(a)
+
+
+######
+#quick sort
+
+def quick_sort(li, left, right):
+    if left < right:
+        mid = partition(li, left, right)
+        quick_sort(li, left, mid -1)
+        quick_sort(li, mid + 1, right)
+
+
+def partition(li, left, right):
+
+    tem = li[left]
+
+    while left < right:
+
+    pass
+
