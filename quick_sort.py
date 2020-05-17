18918252a9997b01aa33ded99164e34feb76a1b0
GHU001 <ghuu001@gmail.com>
2020-05-18 00:33:58 +0800

    Default Changelist

--- quick_sort.py
+++ quick_sort.py
@@ -1,13 +1,18 @@
-li = [1,3,2,4,1,5,5,4,3]
-
-a = 0
-for i in li:
-    a = a ^ i
-print(a)
+# li = [1,3,2,4,1,5,5,4,3]
+#
+# a = 0
+# for i in li:
+#     a = a ^ i
+# print(a)
 
 
 ######
 #quick sort
+import random
+
+li = list(range(1000))
+random.shuffle(li)
+# li.append(1)
 
 def quick_sort(li, left, right):
     if left < right:
@@ -28,7 +33,22 @@ def partition(li, left, right):
         while li[left] < tem and left < right:
             left += 1
         li[right] = li[left]
+    li[left] = tem
     return left
 
-quick_sort(li,0,len(li)-1)
-print(li)
+
+#
+
+
+# quick_sort(li,0,len(li) - 1)
+
+# print(li)
+
+def quick_sort2(li):
+
+    tem = li[0]
+    left_set = [v for v in li[1:] if v < tem]
+    right_set =[v for v in li[1:] if v > tem]
+    left_set = quick_sort2(left_set)
+    right_set= quick_sort2(right_set)
+    return left_set + tem + right_set
