c1a309ad7ad1916cefb5fea2afbc518a2a548456
GHU001 <ghuu001@gmail.com>
2020-09-26 23:19:01 +0800

    Default Changelist

--- Example/数字交换.py
+++ Example/数字交换.py
@@ -24,5 +24,25 @@ print(largest_cont_number(li))
 '''
 活动选择问题
 每个活动都有一个开始时间Si和结束时间fi，安排那些活动能使场地举办的活动最多
+
+贪心算法：
+最先结束的一定是最优解的一部分
+
 '''
+
+activities = [(1,4), (3,5), (0,6), (5,7), (3,9), (5,9),(6,10),(8,11), (8,12), (2,14),(12,16)]
+
+activities.sort(key=lambda x: x[1])
+
+def activities_selection(a):
+    res = [a[0]]
+    for i in range(1, len(a)):
+        if a[i][0] >= res[-1][1]:
+            #活动不冲突
+            res.append(a[i])
+    return res
+
+print(activities_selection(activities))
+
+
 
